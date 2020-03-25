d763dba139b24cc73429f785cdd34ff58b7dba2a
AzusaChino <azusa146@gmail.com>
2020-03-25 20:18:31 +0800

    ft. websocket chat demo

---
+++ hello-netty/src/main/java/cn/az/project/netty/initializer/WebSocketServerInitializer.java
@@ -0,0 +1,38 @@
+package cn.az.project.netty.initializer;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author az
+ * @date 2020/3/25
+ */
+public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
+
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) {
+        ChannelPipeline cp = socketChannel.pipeline();
+
+        // websocket based on http
+        cp.addLast(new HttpServerCodec());
+
+        // support for big data write
+        cp.addLast(new ChunkedWriteHandler());
+
+        // aggregate http message -> FullHttpRequest/FullHttpResponse
+        cp.addLast(new HttpObjectAggregator(1024 * 64));
+
+        // websocket server protocol | route:/ws
+        // helps deal with hand shaking
+        // for web socket, data transferred by frames
+        cp.addLast(new WebSocketServerProtocolHandler("/ws"));
+
+        cp.addLast(new WebSocketServerInitializer());
+    }
+}
---
+++ hello-netty/src/main/java/cn/az/project/netty/websocket/WebSocketServer.java
@@ -0,0 +1,39 @@
+package cn.az.project.netty.websocket;
+
+import cn.az.project.netty.initializer.WebSocketServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author az
+ * @date 2020/3/25
+ */
+@Slf4j
+public class WebSocketServer {
+
+    public static void main(String[] args) {
+
+        EventLoopGroup mainGroup = new NioEventLoopGroup();
+        EventLoopGroup subGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(mainGroup, subGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new WebSocketServerInitializer());
+
+            ChannelFuture cf = bootstrap.bind(8088).sync();
+
+            cf.channel().closeFuture().sync();
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage(), e);
+        } finally {
+            mainGroup.shutdownGracefully();
+            subGroup.shutdownGracefully();
+        }
+    }
+}
