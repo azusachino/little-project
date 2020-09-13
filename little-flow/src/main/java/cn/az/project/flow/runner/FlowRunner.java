81d2078cb2a25e9567f7487123a0f090cad7d6d3
AzusaChino <azusa146@gmail.com>
2020-09-13 22:32:16 +0800

    ft. add something for flowable

---
+++ little-flow/src/main/java/cn/az/project/flow/runner/FlowRunner.java
@@ -0,0 +1,22 @@
+package cn.az.project.flow.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author az
+ * @since 09/12/20
+ */
+@Component
+public class FlowRunner implements CommandLineRunner {
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming main method arguments
+     * @throws Exception on error
+     */
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
